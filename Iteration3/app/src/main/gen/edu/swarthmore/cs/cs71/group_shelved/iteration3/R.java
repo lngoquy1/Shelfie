3b9d122ba829132c921f5109ddeaa30b64ece245
Lan Ngo <lngoquy1@swarthmore.edu>
2017-11-01 15:36:42 -0400

    Remove Iteration3

--- Iteration3/app/src/main/gen/edu/swarthmore/cs/cs71/group_shelved/iteration3/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package edu.swarthmore.cs.cs71.group_shelved.iteration3;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
