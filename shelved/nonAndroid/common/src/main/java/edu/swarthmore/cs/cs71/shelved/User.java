0d956af61b2d748e932b917b5518d44c961e227c
Leah Brumgard <lbrumga1@swarthmore.edu>
2017-11-03 22:21:40 -0400

    edited user

--- shelved/nonAndroid/common/src/main/java/edu/swarthmore/cs/cs71/shelved/User.java
+++ shelved/nonAndroid/common/src/main/java/edu/swarthmore/cs/cs71/shelved/User.java
@@ -8,18 +8,19 @@ public class User {
     private String location;
     private String salt;
 
-    public User(String username, String password, String name, String bio, String location, String salt) {
+    public User(String username, String password, String name, String bio, String location) {
         this.username = username;
         this.password = password;
         this.name = name;
         this.bio = bio;
         this.location = location;
         this.salt = salt;
+
     }
 
     public void changePassword(String oldPassword, String newPassword) {
         // if hash(oldPassword) = this.password {
-        //      this.password = hash(newPassoword)
+        //      this.password = hash(newPassword)
         // } else {
         //      notify user to try again
         // }
