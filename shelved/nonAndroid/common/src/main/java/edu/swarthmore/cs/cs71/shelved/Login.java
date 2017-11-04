bba606c42215875530449218c775cddbc4c970f0
Leah Brumgard <lbrumga1@swarthmore.edu>
2017-11-04 19:34:25 -0400

    added pseudocode to login class:

--- shelved/nonAndroid/common/src/main/java/edu/swarthmore/cs/cs71/shelved/Login.java
+++ shelved/nonAndroid/common/src/main/java/edu/swarthmore/cs/cs71/shelved/Login.java
@@ -3,8 +3,18 @@ package edu.swarthmore.cs.cs71.shelved;
 public class Login {
 
     public void login(String username, String password) {
-        // if username in database
-        // 
+        // if username in database {
+        //     go to user in database
+        //     lookup salt associated with user
+        //     hash given password+salt
+        //     check against hashed password+salt stored in database
+        //     if match {
+        //         login (return user data to have locally?)
+        //     if not match {
+        //         "invalid username or password
+        //     }
+        // } else {
+        //     create new user (hash password, c
     }
 
 }
