a426c0fa8c348c03022e9f7f4b45d213dabf491e
Kellie Dinh <kdinh@cs.swarthmore.edu>
2017-11-16 21:38:27 -0500

    work on shelf

--- shelved/android/app/src/main/java/edu/swarthmore/cs/cs71/shelved/shelved/ShelfFragment.java
+++ shelved/android/app/src/main/java/edu/swarthmore/cs/cs71/shelved/shelved/ShelfFragment.java
@@ -1,15 +1,16 @@
 package edu.swarthmore.cs.cs71.shelved.shelved;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
+import android.widget.*;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class ShelfFragment extends ListFragment implements OnItemClickListener {
@@ -19,32 +20,52 @@ public class ShelfFragment extends ListFragment implements OnItemClickListener {
         return fragment;
     }
 
+    // Array of strings storing country names
+    String[] books = new String[] {
+            "Kafka by the Shore"
+    };
+
+    // Array of integers points to images stored in /res/drawable/
+    int[] imgs = new int[]{
+            R.drawable.shelved_logo
+    };
+
+    // Array of strings to store currencies
+    String[] author = new String[]{
+            "Haruki Murakami"
+    };
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_shelf, container, false);
-        return view;
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+        // Each row in the list stores country name, author and flag
+        List<HashMap<String,String>> aList = new ArrayList<HashMap<String,String>>();
 
-        // Gets array of books - works for simple list item with one text field
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.Books, android.R.layout.simple_list_item_1);
+        for(int i=0;i<10;i++){
+            HashMap<String, String> hm = new HashMap<String,String>();
+            hm.put("txt", "Book : " + books[i]);
+            hm.put("cur","Author : " + author[i]);
+            hm.put("flag", Integer.toString(imgs[i]) );
+            aList.add(hm);
+        }
+
+        // Keys used in Hashmap
+        String[] from = { "flag","txt","cur" };
+
+        // Ids of views in listview_layout
+        int[] to = { R.id.flag,R.id.txt,R.id.cur};
+
+        // Instantiating an adapter to store each items
+        // R.layout.listview_layout defines the layout of each item
+        SimpleAdapter adapter = new SimpleAdapter(getActivity().getBaseContext(), aList, R.layout.book_list_item, from, to);
+
         setListAdapter(adapter);
-        getListView().setOnItemClickListener(this);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-
-
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
     }
 }
