a87fb5401a60de8d1391299a5bb58af4d8636033
Kellie Dinh <kdinh@cs.swarthmore.edu>
2017-11-16 21:00:25 -0500

    work on list view of books

--- shelved/android/app/src/main/java/edu/swarthmore/cs/cs71/shelved/shelved/ShelfFragment.java
+++ shelved/android/app/src/main/java/edu/swarthmore/cs/cs71/shelved/shelved/ShelfFragment.java
@@ -2,15 +2,38 @@ package edu.swarthmore.cs.cs71.shelved.shelved;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 
-public class ShelfFragment extends Fragment {
+public class ShelfFragment extends ListFragment implements OnItemClickListener {
+
     public static ShelfFragment newInstance() {
         ShelfFragment fragment = new ShelfFragment();
         return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_shelf, container, false);
+        return view;
+    }
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Gets array of books - works for simple list item with one text field
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.Books, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
     }
 
     @Override
@@ -18,9 +41,10 @@ public class ShelfFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_shelf, container, false);
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
     }
 }
