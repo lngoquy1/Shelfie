338b1b54793e9b9fcd6a890a9ab7355707520284
Leah Brumgard <lbrumga1@swarthmore.edu>
2017-11-28 21:20:50 -0500

    scanner

--- shelved/android/app/src/main/java/edu/swarthmore/cs/cs71/shelved/shelved/CameraFragment.java
+++ shelved/android/app/src/main/java/edu/swarthmore/cs/cs71/shelved/shelved/CameraFragment.java
@@ -1,14 +1,20 @@
 package edu.swarthmore.cs.cs71.shelved.shelved;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class CameraFragment extends Fragment {
+    private static final String TAG = "CameraFragment";
+
     public static CameraFragment newInstance() {
         CameraFragment fragment = new CameraFragment();
+        Context context;
         return fragment;
     }
 
@@ -20,6 +26,14 @@ public class CameraFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_camera, container, false);
+        Context context;
+        View rootView = inflater.inflate(R.layout.fragment_camera, container, false);
+        context = rootView.getContext();
+        Intent intent = new Intent(context, ScannerActivity.class);
+        Log.d(TAG, "about to call activity start");
+        startActivity(intent);
+        Log.d(TAG, "past activity start");
+        return rootView;
+        //return inflater.inflate(R.layout.fragment_camera, container, false);
     }
 }
--- shelved/android/app/src/main/java/edu/swarthmore/cs/cs71/shelved/shelved/ScannerActivity.java
+++ shelved/android/app/src/main/java/edu/swarthmore/cs/cs71/shelved/shelved/ScannerActivity.java
@@ -1,4 +1,80 @@
 package edu.swarthmore.cs.cs71.shelved.shelved;
 
-public class ScannerActivity {
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import me.dm7.barcodescanner.zbar.Result;
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
+
+public class ScannerActivity extends Activity implements ZBarScannerView.ResultHandler {
+    private static final String TAG = "ScannerActivity";
+    private ZBarScannerView mScannerView;
+
+    @Override
+    public void onCreate(Bundle state) {
+        Log.d(TAG, "inside onCreate for scanner activity");
+        super.onCreate(state);
+        setContentView(R.layout.activity_scanner);
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA}, 0);
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mScannerView != null) {
+            mScannerView.setResultHandler(this);
+            mScannerView.startCamera();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mScannerView != null) {
+            mScannerView.stopCamera();
+        }
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        Toast.makeText(this, "Contents = " + rawResult.getContents() +
+                ", Format = " + rawResult.getBarcodeFormat().getName(), Toast.LENGTH_SHORT).show();
+        // Note:
+        // * Wait 2 seconds to resume the preview.
+        // * On older devices continuously stopping and resuming camera preview can result in freezing the app.
+        // * I don't know why this is the case but I don't have the time to figure out.
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mScannerView.resumeCameraPreview(ScannerActivity.this);
+            }
+        }, 2000);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String permissions[], int[] grantResults) {
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            ViewGroup contentFrame = (ViewGroup) findViewById(R.id.scannerLayout);
+            mScannerView = new ZBarScannerView(this);
+            contentFrame.addView(mScannerView);
+            mScannerView.setResultHandler(this);
+            mScannerView.startCamera();
+        } else {
+            Toast.makeText(this, "Please grant camera permission to use the QR Scanner", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
