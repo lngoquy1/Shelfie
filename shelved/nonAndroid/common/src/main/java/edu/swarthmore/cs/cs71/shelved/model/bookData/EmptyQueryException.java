716fe605092ed0ae338f4a3b0d68a53ed240db34
Nicole Feldbaum <nfeldba1@swarthmore.edu>
2017-11-30 19:06:18 -0500

    think we're done with bookinfo parsing???

---
+++ shelved/nonAndroid/common/src/main/java/edu/swarthmore/cs/cs71/shelved/model/bookData/EmptyQueryException.java
@@ -0,0 +1,8 @@
+package edu.swarthmore.cs.cs71.shelved.model.bookData;
+
+public class EmptyQueryException extends Exception {
+    public EmptyQueryException(String message){
+        super(message);
+    }
+
+}
